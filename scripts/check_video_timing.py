d292c20b1a4035d50a5aeafac2c1ff397c3a4796
julian-WIS <julian@wildlifeimagingsystems.com>
2025-07-24 12:20:48 -0400

    Organize based on camera night

--- scripts/check_video_timing.py
+++ scripts/check_video_timing.py
@@ -2,35 +2,71 @@ from tqdm import tqdm
 import numpy as np
 from pathlib import PurePath
 import pandas as pd
-from datetime import datetime
+from datetime import datetime, timedelta
 import matplotlib.pyplot as plt
+import plotly.express as px
+
 
 def create_heatmap(count_detect_files_array, date_list, config, camera_ids):
-    MIN_VAL = 0
+    # Create a heatmap of the number of video files detected per camera and date
+    heatmap_df = pd.DataFrame(
+        count_detect_files_array,
+        index=camera_ids,
+        columns=[str(d.strftime('%Y - %m - %d')) for d in date_list]  # Convert dates to str for Plotly 
+    )
+
+    # drop last camera night if there are now videos
+    if all(heatmap_df[heatmap_df.columns[-1]] == 0):
+        heatmap_df.drop(columns=[heatmap_df.columns[-1]], inplace=True)
+
+
     MAX_VAL = count_detect_files_array.max()
-    fig = plt.figure(figsize=[12,4],facecolor='gray')
-    ax = fig.add_subplot()
-    im = ax.imshow(count_detect_files_array,cmap='inferno',vmin=MIN_VAL,vmax=MAX_VAL)
-    ax.set_xticks(np.arange(len(date_list)))
-    ax.set_xticklabels(date_list)
-    plt.xlabel('Date')
     location, project, datapull = config.location, config.project, config.datapull
-    plt.title(f'{project},{location}.{datapull}\nNumber of MKV Files for Each Date ({MAX_VAL} maximum)')
-    plt.setp(ax.get_xticklabels(), rotation=-90, ha="left",
-            rotation_mode="anchor",fontsize='xx-small')
 
-    ax.set_yticks(np.arange(len(camera_ids)))
-    ax.set_yticklabels(camera_ids)
-    ax.set_ylim((len(camera_ids)-0.5, -0.5))
-    # ax.set_position([0.02,0.2,0.95,0.8])
-    plt.ylabel('Turbine')
-    ##Rotate the tick labels and set their alignment.
-    plt.setp(ax.get_yticklabels(),ha="right",fontsize='xx-small')
+    heatmap = px.imshow(
+        heatmap_df,
+        labels=dict(x="Date", y="Camera ID", color="File Count"),
+        aspect="auto",
+        color_continuous_scale="Purples",
+        zmin=0,
+        zmax=count_detect_files_array.max()
+    )
+    heatmap.update_xaxes(
+        tickfont=dict(size=12),
+        side="bottom",
+        tickangle=90
+    )
+    heatmap.update_yaxes(
+        tickfont=dict(size=12)
+    )
 
-    fig.colorbar(im, orientation="vertical", pad = 0.05,label='# Files For Each Date')
-    plt.tight_layout()
-    plt.show()
+    # Add a title to the plot
+    heatmap.update_layout(
+        title_text=f"{project} - {location} - {datapull}<br><sup>Video Count per Day ({MAX_VAL} maximum)</sup>",
+        title_font_size=20,
+        title_subtitle_font_size=14,
+        title_subtitle_font_color="gray"
+    )
+    heatmap.show()
 
+def get_files_in_camera_night(paths, date):
+    in_camera_night = []
+
+    for path in paths:
+    # parse files date
+        date_str = ' '.join(PurePath(path).parts[-1].split('_')[:-1])
+        try:
+            path_date = datetime.strptime(date_str, "%Y%m%d %H%M%S")
+            if path_date.date() == date:
+                if path_date.hour > 12:
+                    in_camera_night.append(path)
+            elif path_date.date() == date + timedelta(days=1):
+                if path_date.hour <= 12:
+                    in_camera_night.append(path)
+        except ValueError as e:
+            pass
+
+    return in_camera_night
 
 def create_video_timing_card(config):
     all_files = config.data_access.list_video_paths()
@@ -53,11 +89,11 @@ def create_video_timing_card(config):
             camera_video_files = config.data_access.list_video_paths(camera_id)
             camera_xml_files = config.data_access.list_xml_paths(camera_id)
             for ind_date, date in enumerate(date_list):
-                date_str = datetime.strftime(date, "%Y%m%d")
-
-                file_list = [path for path in camera_video_files if date_str in path]
+                file_list = get_files_in_camera_night(camera_video_files, date)
                 count_detect_files_array[ind_camera_id,ind_date] = len(file_list)
-                xml_list = [path for path in camera_xml_files if date_str in path]
+
+
+                xml_list = get_files_in_camera_night(camera_xml_files, date)
                 xml_list = [path for path in xml_list if 'recording' not in path]
                 count_xmls[ind_camera_id,ind_date] = len(xml_list)
 
